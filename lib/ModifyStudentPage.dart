f9cf27c74f76b12cc7f4891e5e4557fdaeaeebeb
BrianawhyWh <22100289@handong.edu>
2022-01-26 17:53:29 +0900

    Modify ModifyStudentPage.

--- lib/ModifyStudentPage.dart
+++ lib/ModifyStudentPage.dart
@@ -16,100 +16,212 @@ class ModifyStudentPage extends StatelessWidget {
           elevation: 0.0,
           leading: IconButton(
             icon: SvgPicture.asset("assets/Icons/BackButton.svg"),
-            onPressed: () {
-
-            },
+            onPressed: () {},
           ),
         ),
-        body:
-
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget> [
-            const SizedBox(
-              height: 50.0,
-              width: 100.0,
-            ),
-            Row(
-              children: [
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  decoration: new BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2.0),
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-                SizedBox(
-                  width: 15.0,
-                ),
-                Column(
-                  children: [
-                    Text('이름: 김시선',
-                      style: TextStyle(
-                        fontFamily: "DoHyeon",
-                        fontSize: 25.0,
-                        color: Color(0xFF000000),
-                      ),
-                    ),
-                    Text('생년월일:2007/10/04',
-                      style: TextStyle(
-                        fontFamily: "DoHyeon",
-                        fontSize: 25.0,
-                        color: Color(0xFF000000),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 25.0,
-            ),
-            Container(
-              height: 300,
-              width: 350,
-              decoration: BoxDecoration(
-                  color: Color(0xFFF2F2F2)
-              ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
               child: Column(
-                children: [
-                  Text('현재 김시선의 상태는?',
-                    style: TextStyle(
-                      fontFamily: "DoHyeon",
-                      fontSize: 30.0,
-                      color: Color(0xFF000000),
+                children: <Widget>[
+                  const SizedBox(
+                    height: 30.0,
+                    width: 100.0,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100.0,
+                        height: 100.0,
+                        decoration: new BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2.0),
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            '이름: 김시선',
+                            style: TextStyle(
+                              fontFamily: "DoHyeon",
+                              fontSize: 25.0,
+                              color: Color(0xFF000000),
+                            ),
+                          ),
+                          Text(
+                            '생년월일:2007/10/04',
+                            style: TextStyle(
+                              fontFamily: "DoHyeon",
+                              fontSize: 25.0,
+                              color: Color(0xFF000000),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    height: 280,
+                    width: 350,
+                    decoration: BoxDecoration(color: Color(0xFFF2F2F2)),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          '현재 김시선의 상태는?',
+                          style: TextStyle(
+                            fontFamily: "DoHyeon",
+                            fontSize: 30.0,
+                            color: Color(0xFF000000),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                icon:
+                                SvgPicture.asset("assets/Icons/SadButton.svg"),
+                                iconSize: 60.0,
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon:
+                                SvgPicture.asset("assets/Icons/SosoButton.svg"),
+                                iconSize: 60.0,
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: SvgPicture.asset(
+                                    "assets/Icons/SmileButton.svg"),
+                                iconSize: 60.0,
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: SvgPicture.asset(
+                                    "assets/Icons/AngryButton.svg"),
+                                iconSize: 60.0,
+                                onPressed: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Content(),
+                      ],
                     ),
                   ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    height: 200,
+                    width: 350,
+                    decoration: BoxDecoration(color: Color(0xFFF2F2F2)),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          '칭찬해주세요:)',
+                          style: TextStyle(
+                            fontFamily: "DoHyeon",
+                            fontSize: 30.0,
+                            color: Color(0xFF000000),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        SecondContent(),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
 
-                  Row(
-                    children:[
-                      IconButton(
-                        icon: SvgPicture.asset("assets/Icons/SadButton,svg"),
-                        iconSize: 50.0,
-                        onPressed: () {},
-                      ),
-                      Text('현재 김시선의 상태는?',
-                        style: TextStyle(
-                          fontFamily: "DoHyeon",
-                          fontSize: 30.0,
-                          color: Color(0xFF000000),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print('Elevated button');
+                          },
+                          child: Text('저장', style: TextStyle(
+                            fontFamily: "DoHyeon",
+                            fontSize: 30.0,
+                            color: Color(0xFF000000),
+                          ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              elevation: 0.0,
+                              primary: Color(0xFFDDDDDF),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0)
+                              )
+                          ),
                         ),
                       ),
                     ],
                   ),
-
-
                 ],
-
-
               ),
             ),
           ],
         ),
-
       ),
     );
   }
+
+  Widget Content() {
+    return Expanded(
+      child: TextFormField(
+        maxLines: 2,
+        textAlign: TextAlign.center,
+        decoration: const InputDecoration(
+          fillColor: Colors.grey,
+          labelText: "왜 그런 기분인지 간략하게 설명해주세요.(100자 내외)",
+          labelStyle: TextStyle(
+              fontSize: 12
+          ),
+          border: OutlineInputBorder(
+            //borderSide: BorderSide(width: 1,color: Colors.redAccent),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget SecondContent() {
+  return TextFormField(
+    maxLines: 2,
+    textAlign: TextAlign.center,
+    decoration: const InputDecoration(
+      fillColor: Colors.grey,
+      labelText: "오늘 자녀에게 칭찬해주고 싶은 점이 있나요?(200자 내외)",
+      labelStyle: TextStyle(
+          fontSize: 12
+      ),
+      border: OutlineInputBorder(
+        //borderSide: BorderSide(width: 1,color: Colors.redAccent),
+      ),
+    ),
+  );
 }
