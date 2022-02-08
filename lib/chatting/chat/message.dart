6e1ef56be1846269b026d54ccd7d7a552b82d9a2
isin-won <leeshin1130@gmail.com>
2022-02-08 18:42:12 +0900

    connect firebase user collection

---
+++ lib/chatting/chat/message.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      //stream: FirebaseFirestore.instance.collection('chat'),
+        builder: builder,
+    );
+  }
+}
--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:fireproject/start_page.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
