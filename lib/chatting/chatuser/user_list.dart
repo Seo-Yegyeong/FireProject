153e1b09095948d9cdaa2622392a738c9ece542d
BrianawhyWh <22100289@handong.edu>
2022-02-11 18:00:34 +0900

    update user

---
+++ lib/chatting/chatuser/user_list.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fireproject/chatting/chat.dart';
+import 'package:fireproject/chatting/chatuser/user_card.dart';
+import 'package:flutter/material.dart';
+
+import '../chat/chat_screen.dart';
+
+class UserList extends StatefulWidget {
+  const UserList({Key? key}) : super(key: key);
+
+
+  @override
+  _UserListState createState() => _UserListState();
+}
+
+class _UserListState extends State<UserList> {
+  final user = FirebaseAuth.instance.currentUser;
+  CollectionReference database = FirebaseFirestore.instance.collection('chat');
+  late QuerySnapshot querySnapshot;
+  late List<Chat> chatInfo =[];
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+
+      builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot){
+        if(snapshot.connectionState==ConnectionState.waiting){
+          return const Center(
+            child:CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasData) {
+          final chatDocs = snapshot.data!.docs;
+
+          for (int i = 0; i < chatDocs.length; i++) {
+            var a = chatDocs[i];
+            if (chatDocs[i]['sendId'] == user!.uid) {
+              Timestamp t = chatDocs[i]['time'];
+              String time = DateTime.fromMicrosecondsSinceEpoch(t.microsecondsSinceEpoch).toString().split(" ")[0];
+              Chat chat = Chat(chatDocs[i]['text'],chatDocs[i]['sendId'],chatDocs[i]['takeId'],time);
+
+              chatInfo.add(chat);
+            }
+          }
+        }
+
+        return ListView.builder(
+          itemCount: chatInfo.length,
+          itemBuilder: (context, index){
+
+            return GestureDetector(
+              onTap: (){
+
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ChatScreen(chatInfo[index].takeId),
+                  ),
+                );
+              },
+                child: UserCard(
+                  chatInfo[index],
+                ),
+
+            );
+
+          },
+
+        );
+      },
+    );
+  }
+
+}
---
+++ lib/chatting/chatuser/user_screen.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fireproject/chatting/chatuser/user_list.dart';
+import 'package:flutter/material.dart';
+
+class UserScreen extends StatefulWidget {
+  UserScreen({Key? key}) : super(key: key);
+
+
+  @override
+  _UserScreenState createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  final _authentication = FirebaseAuth.instance;
+  User? loggedUser;
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Chat Screen'),
+
+        titleTextStyle: const TextStyle(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.exit_to_app_sharp,
+              color: Colors.amber,
+            ),
+            onPressed: (){
+              _authentication.signOut();
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+
+      body: Container(
+        child: Column(
+            children: const [
+              Expanded(
+                child: UserList(),
+              ),
+
+            ]
+        ),
+      ),
+    );
+  }
+}
