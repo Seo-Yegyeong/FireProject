bf550109f4be3b83f33cc82a93776ffa6fe08dd7
Lab <seoye6618@gmail.com>
2022-02-11 17:31:19 +0900

    [FEAT] temporary commit..2

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
 
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/bottom',
+      initialRoute: '/auth',
       getPages: [
         GetPage(
           name: '/login',
--- lib/src/pages/announcement/announce_detail_page.dart
+++ lib/src/pages/announcement/announce_detail_page.dart
@@ -32,21 +32,124 @@ class AnnounceDetailPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        // children: [
-        //   Text("TEST: id is $id \n\n$data"),
-        //   Text("글 제목"),
-        //   ElevatedButton(
-        //     onPressed: () {},
-        //     child: Text("삭제"),
-        //   )
-        //   ElevatedButton(
-        //     onPressed: () {},
-        //     child: Text("수정"),
-        //   )
-        //
-        //   Expanded(child: SingleChildScrollView( child: Text("글 내용"),))
-        // ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            children: [
+              //Text("TEST: id is $id \n\n$data"),
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      color: Color(0xFFFFC700),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      "김한동",
+                      style: TextStyle(fontFamily: "DoHyeonFont", fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 30,
+                thickness: 1,
+                color: Colors.black,
+              ),
+              Expanded(
+                  child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(
+                      "글 제목",
+                      style: TextStyle(fontSize: 25, fontFamily: "DoHyeonFont"),
+                    ),
+                    //SizedBox(height: 5,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "작성 일시   ",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("글 내용" * 100),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          height: 130,
+                          width: 130,
+                          color: Colors.lightGreenAccent,
+                          child: Text(
+                            "사진 넣을 자리",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          height: 130,
+                          width: 130,
+                          color: Colors.lightGreenAccent,
+                          child: Text(
+                            "사진 넣을 자리",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          height: 130,
+                          width: 130,
+                          color: Colors.lightGreenAccent,
+                          child: Text(
+                            "사진 넣을 자리",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      height: 30,
+                      thickness: 1,
+                      color: Colors.black,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("좋아요랑 댓글개수 표시"),
+                      ],
+                    ),
+                    Container(
+                      width: getScreenWidth(context),
+                      height: 300,
+                      color: Colors.orange,
+                      child: Center(
+                        child: Text(
+                          "댓글 부분입니당",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+            ],
+          ),
+        ),
       ),
     );
   }
